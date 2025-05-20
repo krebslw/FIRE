1021fa62c9b3ff6953b482f939b12c803cdd53eb
Stefan Krebs <stkre@sdfi.dk>
2025-05-20 16:51:33 +0200

    Ret imports: tilføj fire.api.*

--- fire/api/geodetic_levelling/__init__.py
+++ fire/api/geodetic_levelling/__init__.py
@@ -2,12 +2,12 @@
 geodetic_levelling
 """
 
-import geodetic_levelling.contour_plot
-import geodetic_levelling.geodetic_correction_levelling_obs
-import geodetic_levelling.histogram
-import geodetic_levelling.metric_to_gpu_transformation
-import geodetic_levelling.tidal_transformation
-import geodetic_levelling.time_propagation
-import geodetic_levelling.geophysical_parameters
+import fire.api.geodetic_levelling.contour_plot
+import fire.api.geodetic_levelling.geodetic_correction_levelling_obs
+import fire.api.geodetic_levelling.histogram
+import fire.api.geodetic_levelling.metric_to_gpu_transformation
+import fire.api.geodetic_levelling.tidal_transformation
+import fire.api.geodetic_levelling.time_propagation
+import fire.api.geodetic_levelling.geophysical_parameters
 
 __version__ = "1.0.0"
