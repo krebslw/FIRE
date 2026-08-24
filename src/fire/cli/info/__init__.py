f153f13323ad176572c80e15c9785e8ad78c0e15
Stefan Krebs <stkre@sdfi.dk>
2026-08-24 17:46:41 +0200

    fire info: Tilføj interaktiv session
    Start en interaktiv session med alle subkommandoer under `fire info`
    Vælges med flaget `-I/--interaktiv`.
    
    ```
    >>fire info -I punkt
    Starter interaktiv session med 'fire info punkt'
    Afbryd med CTRL+C
    fire info punkt <BRUGER INPUT>
    ```
    
    eller
    
    ```
    >>fire info -I infotype
    Starter interaktiv session med 'fire info infotype'
    Afbryd med CTRL+C
    fire info infotype <BRUGER INPUT>
    ```
    
    Brugeren promptes kun for argumenter og options til den valgte kommando,
    og slipper dermed for fx at skulle skrive "fire info punkt" hver gang.
    
    En anden stor fordel er, at FIRE har en væsentlig overhead ifm. import
    af store afhængigheder såsom sqlalchemy og pandas foruden et efter-
    hånden ret omfattende API-lag. I en interaktiv session hentes alle
    imports kun én gang i starten. Dermed skal brugeren kun vente på selve
    kommando-kaldet og databaseforespørgslen.

--- src/fire/cli/info/__init__.py
+++ src/fire/cli/info/__init__.py
@@ -1,13 +1,58 @@
 print("importerede cli.info.__init__")
+import shlex
+
 import click
+from fire.cli.exceptions import AfbrydFejl
 
 
 @click.group()
-def info():
+@click.pass_context
+@click.option(
+    "-I",
+    "--interaktiv",
+    help=f"Start en interaktiv session op",
+    is_flag = True,
+    required=False,
+    type = bool,
+)
+def info(ctx: click.Context, interaktiv: bool):
     """
     Information om objekter i FIRE
     """
-    pass
+
+    if not interaktiv:
+        return
+
+    kommando: str = ctx.invoked_subcommand
+
+    # Hvis kommando ikke er en gyldig sub-kommando af fire info,
+    # vil det blive fanget længere oppe, så burde ikke være nogen
+    # fare for KeyError her.
+    subkommando: click.Command = ctx.command.commands[kommando]
+
+    # TODO: gør så brugeren fx kun skal specificere options én gang.
+    # Så man heller gang ikke skal skrive fx:
+    # -DHOalle -Kts,alle --db prod <FIKSPUNKTSNR>
+    print(f"Starter interaktiv session med 'fire info {kommando}'")
+    print(f"Afbryd med CTRL+C")
+    while True:
+
+        brugerinput = click.prompt(f"fire info {kommando} ", prompt_suffix = "")
+        try:
+            # Lav en ny kontekst baseret på oprindelig kontekst og brugerinput
+            # Brugerinput splittes og parsing overlades til click's make_context
+            ny_ctx = subkommando.make_context(
+                info_name = f"Interaktiv version af {kommando}",
+                args = shlex.split(brugerinput, " "),
+            )
+
+            ny_ctx.command.callback(**ny_ctx.params)
+        except Exception as exc:
+            print(exc)
+        except AfbrydFejl as exc:
+            # SystemExits, smidt igennem AfbrydFejl bliver også fanget og printet (inkl
+            # click formattering)
+            print(exc)
 
 
 # Udstil kommandoer
